eeff7a2f6140e2358adcf66f2c8bf37878a4a6dc
ramp4578 <ramprasadp47@gmail.com>
2023-09-11 17:30:49 +0530

    render components and pass properties one by one is done

--- src/index.js
+++ src/index.js
@@ -1,13 +1,46 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import './index.css'
 
-const Greeting = () => {
-  return <h1>My First comp</h1>
+const books = [
+  {
+    title: 'Chanakyas Chant, Bharat Series 2 ',
+    author: 'Jordan Moore',
+    img: './images/book1.jpg',
+    id: 1,
+  },
+  {
+    author: 'James Clear',
+    title: 'Atomic Habits',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg',
+    id: 2,
+  },
+]
+
+function BookList() {
+  return (
+    <section className="bookList">
+      {books.map((book) => {
+        console.log(book)
+        const { img, title, author } = book
+
+        return <Book img={img} title={title} author={author} />
+      })}
+    </section>
+  )
 }
 
-function name() {
-  return <h2>Hello Ram</h2>
+const Book = ({ img, author, title, children }) => {
+  return (
+    <article className="book">
+      <img src={img} alt={title} />
+      {children}
+      <h2>{title}</h2>
+      <h4>{author}</h4>
+    </article>
+  )
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<Greeting />)
+
+root.render(<BookList />)
--- src/testing.js
+++ src/testing.js
@@ -1,7 +1,27 @@
 import React from 'react'
+import ReactDOM from 'react-dom/client'
 
-const testing = () => {
-  return <div>testing</div>
+const Greetings = () => {
+  return (
+    <div>
+      <Person />
+      <Message />
+      <Name />
+    </div>
+  )
 }
 
-export default testing
+const Person = () => {
+  return <h2>Ramprasad Pendmi</h2>
+}
+
+const Message = () => {
+  return <p>Here is the message</p>
+}
+
+function Name() {
+  return <h2>Hello Ram</h2>
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<Greetings />)
